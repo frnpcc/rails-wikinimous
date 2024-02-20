c9fa5527ccd969d824f91ab8b52b25d1c4554bd2
Francesca Correa <francescacciavaglia@gmail.com>
2024-02-20 20:19:18 +0100

    app running

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,15 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+require 'faker'
+
+puts "Creating 10 fake articles..."
+
+10.times do
+  article = Article.new(
+    title: Faker::Lorem.sentence,
+    content: Faker::Lorem.paragraph
+  )
+  article.save!
+end
+puts "Finished!"
